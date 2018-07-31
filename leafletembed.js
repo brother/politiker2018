d1e74f5255db2ca0c7450d78dd22fa5c3a0d24b2
Martin Bagge <martin.bagge@glesys.se>
2018-07-31 11:47:43 +0200

    Initial map installment.

---
+++ leafletembed.js
@@ -0,0 +1,19 @@
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+function initmap() {
+	// set up the map
+	map = new L.Map('map');
+
+	// create the tile layer with correct attribution
+	var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+
+	// Map center start
+	map.setView(new L.LatLng(57.1809, 12.3960),11);
+	map.addLayer(osm);
+
+}
