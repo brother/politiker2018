b2c4a83dda8a2cb2ae3734b98fbba86f13c347fd
Robert Blomberg <rblomberg@sandvine.com>
2018-08-16 21:49:11 +0200

    Fix variables for kd, again.

--- leafletembed.js
+++ leafletembed.js
@@ -19,9 +19,9 @@ function initmap() {
 //	var partyAFF = loadMarkersForAFF();
 	var partyC = loadMarkersForC();
 //	var partyK = loadMarkersForK();
-	var partyKD = loadMarkersForKD();
+	var partyKD = loadMarkersForKd();
 //	var partyL = loadMarkersForL();
-//	var partyMP = loadMarkersForMP();
+	var partyMP = loadMarkersForMp();
 	var partyM = loadMarkersForM();
 	var partyS = loadMarkersForS();
 //	var partySD = loadMarkersForSD();
@@ -42,7 +42,7 @@ function initmap() {
 
 	var parties = {
 	    "<img width=8% src=\"gfx/logo_c.png\"> Centerpartiet, 4": partyC,
-            "<img width=8% src=\"gfx/logo_m.png\"> Kristdemokraterna, 13": partyKD,
+            "<img width=8% src=\"gfx/logo_m.png\"> Kristdemokraterna, 13": partyKd,
 	    "<img width=8% src=\"gfx/logo_m.png\"> Moderaterna, 13": partyM,
             "<img width=8% src=\"gfx/logo_m.png\"> Socialdemokraterna, 13": partyS,
 	};
