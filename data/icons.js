c1b14d2392bf8b7c58e8151aa14dac404b2d0b00
Martin Bagge <martin.bagge@glesys.se>
2018-08-21 10:00:37 +0200

    Add city dependent data directory.

--- data/icons.js
+++ data/icons.js
@@ -1,57 +1,57 @@
 var icons = [
 	{
 		"id": "aff",
-		"url": "gfx/logo_aff.png",
+		"url": "../gfx/logo_aff.png",
 		"size": [25, 20]
 	},
 	{
 		"id": "c",
-		"url": "gfx/logo_c.png",
+		"url": "../gfx/logo_c.png",
 		"size": [24, 25]
 	},
 	{
 		"id": "k",
-		"url": "gfx/logo_k.png",
+		"url": "../gfx/logo_k.png",
 		"size": [25, 29]
 	},
 	{
 		"id": "kd",
-		"url": "gfx/logo_kd.jpg",
+		"url": "../gfx/logo_kd.jpg",
 		"size": [25, 23]
 	},
 	{
 		"id": "l",
-		"url": "gfx/logo_l.png",
+		"url": "../gfx/logo_l.png",
 		"size": [21, 25]
 	},
 	{
 		"id": "mp",
-		"url": "gfx/logo_mp.png",
+		"url": "../gfx/logo_mp.png",
 		"size": [25, 22]
 	},
 	{
 		"id": "m",
-		"url": "gfx/logo_m.png",
+		"url": "../gfx/logo_m.png",
 		"size": [25, 17]
 	},
 	{
 		"id": "s",
-		"url": "gfx/logo_s.png",
+		"url": "../gfx/logo_s.png",
 		"size": [25, 26]
 	},
 	{
 		"id": "sd",
-		"url": "gfx/logo_sd.png",
+		"url": "../gfx/logo_sd.png",
 		"size": [23, 25]
 	},
 	{
 		"id": "spi",
-		"url": "gfx/logo_spi.png",
+		"url": "../gfx/logo_spi.png",
 		"size": [25, 25]
 	},
 	{
 		"id": "v",
-		"url": "gfx/logo_v.png",
+		"url": "../gfx/logo_v.png",
 		"size": [25, 25]
 	},
 ];
