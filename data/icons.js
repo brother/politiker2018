e326153451e6a61f9f5c009fdf6f7f18d1432c8d
Martin Bagge <martin.bagge@glesys.se>
2018-08-29 00:02:34 +0200

    Added icons for minor parties in Halmstad.

--- data/icons.js
+++ data/icons.js
@@ -10,6 +10,11 @@ var icons = [
 		"size": [24, 25]
 	},
 	{
+		"id": "dd",
+		"url": "../gfx/logo_dd.png",
+		"size": [25, 25]
+	},
+	{
 		"id": "k",
 		"url": "../gfx/logo_k.png",
 		"size": [25, 29]
@@ -35,6 +40,16 @@ var icons = [
 		"size": [25, 17]
 	},
 	{
+		"id": "med",
+		"url": "../gfx/logo_med.png",
+		"size": [25, 24]
+	},
+	{
+		"id": "rnp",
+		"url": "../gfx/logo_rnp.jpg",
+		"size": [25, 19]
+	},
+	{
 		"id": "s",
 		"url": "../gfx/logo_s.png",
 		"size": [25, 26]
@@ -47,6 +62,11 @@ var icons = [
 	{
 		"id": "spi",
 		"url": "../gfx/logo_spi.png",
+		"size": [25, 25]
+	},
+	{
+		"id": "tp",
+		"url": "../gfx/logo_tp.jpg",
 		"size": [25, 25]
 	},
 	{
