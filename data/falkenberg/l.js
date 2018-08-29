626125f3a5614cc3d01acd4cd0b52ab4004182ce
Martin Bagge <martin.bagge@glesys.se>
2018-08-29 23:05:30 +0200

    L done.

--- data/falkenberg/l.js
+++ data/falkenberg/l.js
@@ -1,8 +1,19 @@
-//2
 function loadMarkersForL() {
-	var l = L.marker([10,10], {icon: partyIcons["l"]})
-		.bindPopup("1. <b>Charlotte Persson</b>, Undersköterska");
+	var lLarsFagerstrom = L.marker([56.8975,12.5039], {icon: partyIcons["l"]})
+		.bindPopup("1. <b>Lars Fagerström</b>, 72<br>Lärare<br>Falkenberg");
+	var lJonathanEsbjornsson = L.marker([56.8773,12.5219], {icon: partyIcons["l"]})
+		.bindPopup("2. <b>Jonathan Esbjörnsson</b>, 33<br>IT-konsult<br>Falkenberg");
+	var lLottaJohansson = L.marker([56.9010,12.4832], {icon: partyIcons["l"]})
+		.bindPopup("3. <b>Lotta Johansson</b>, 30<br>Företagare<br>Falkenberg");
+	var lNiclasErlandsson = L.marker([56.8168,12.6229], {icon: partyIcons["l"]})
+		.bindPopup("4. <b>Niclas Erlandsson</b>, 47<br>Ordförande Studieförbundet Vuxenskolan<br>Ugglarp");
+	var lLenaEngstrom = L.marker([56.8944,12.5268], {icon: partyIcons["l"]})
+		.bindPopup("5. <b>Lena Engström</b>, 75<br>Lärare<br>Falkenberg");
 
-	return L.layerGroup([l
+	return L.layerGroup([lLarsFagerstrom,
+						 lJonathanEsbjornsson,
+						 lLottaJohansson,
+						 lNiclasErlandsson,
+						 lLenaEngstrom
 						]);
 }
