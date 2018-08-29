9cba79dedf7719dff6a4c5fe22e9dd2080d6a398
Martin Bagge <martin.bagge@glesys.se>
2018-08-29 23:17:00 +0200

    KD done.

--- data/falkenberg/kd.js
+++ data/falkenberg/kd.js
@@ -1,8 +1,19 @@
-//2
 function loadMarkersForKD() {
-	var kd = L.marker([10,10], {icon: partyIcons["kd"]})
-		.bindPopup("1. <b>Charlotte Persson</b>, Undersköterska");
+	var kdGeorgiaFerris = L.marker([56.9115,12.4896], {icon: partyIcons["kd"]})
+		.bindPopup("1. <b>Georgia Ferris</b>, 36<br>Innesäljare<br>Falkenberg");
+	var kdEmilyCrowe = L.marker([56.9008,12.4914], {icon: partyIcons["kd"]})
+		.bindPopup("2. <b>Emily Crowe</b>, 38<br>Studerande<br>Falkenberg");
+	var kdAnitaAndersson = L.marker([56.9311,12.3648], {icon: partyIcons["kd"]})
+		.bindPopup("3. <b>Anita Andersson</b>, 73x<br>f.d. Verksamhetschef<br>Glommen");
+	var kdSamuelHansson = L.marker([56.8872,12.6423], {icon: partyIcons["kd"]})
+		.bindPopup("4. <b>Samuel Hansson</b>, 19<br>Ordförande KDU Halland<br>Heberg");
+	var kdDanielBernhardt = L.marker([56.8978,12.4791], {icon: partyIcons["kd"]})
+		.bindPopup("5. <b>Daniel Bernhardt</b>, 39<br>Enhetschef<br>Falkenberg");
 
-	return L.layerGroup([kd
+	return L.layerGroup([kdGeorgiaFerris,
+						 kdEmilyCrowe,
+						 kdAnitaAndersson,
+						 kdSamuelHansson,
+						 kdDanielBernhardt
 						]);
 }
